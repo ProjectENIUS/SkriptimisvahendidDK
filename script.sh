044331a912ad39356b49b657612c11b07b3e113f
BASEWOOD <127052755+ProjectENIUS@users.noreply.github.com>
2025-10-17 07:25:45 +0000

    [PATCH] ADD script.sh Documentation
    Added script documentation

--- script.sh
+++ script.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+#
+# SKRIPT: script.sh
+# EESMÄRK: Kontrollib lokaalse veebiserveri (http://localhost/) HTTP
+#          staatuskoodi.
+# Kasutab standardseid "kuldstandard" meetodeid ainult koodi eraldamiseks.
+# AUTOR: Donat Kauler
+# KUUPÄEV: 2025-10-17
+# ASUTUS/ORGANISATSIOON: Skriptimise Tund
+#
+# SELGITUS:
+# 1. Loob unikaalse logifaili nimega, mis sisaldab kuupäeva ja kellaaega
+# 2. Teostab HTTP päringu aadressile http://localhost/ kasutades curl
+# 3. Eraldab HTTP staatuskoodi (-w "%{http_code}")
+# 4. Kui staatuskood on 200, väljastab "TEST OK", muul juhul "TEST VIGA"
+# 5. Logib tulemuse logifaili koos ajatempliga
+№
 
 LOGFILE="webtest_$(date +%Y%m%d_%H%M%S).log"
 
